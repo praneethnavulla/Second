f2f579469dff1c631478427b8e6cffd94ddd4b18
Praneeth <npraneeth18@gmail.com>
2020-12-18 10:22:33 +0530

    added a print statement

--- Myproject/src/Repo/FirstRepo.java
+++ Myproject/src/Repo/FirstRepo.java
@@ -4,6 +4,8 @@ public class FirstRepo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello everyone");
+		System.out.println(" everyone");
 
 	}
 
